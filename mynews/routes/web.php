af25ff382d9b6e8f9836690b6a9de56fbb180a1a
”YuYuYuhei” <yuuhei.kubo7@gmail.com>
2019-02-27 10:09:57 +0900

    PHP/Laravel10-homework

--- mynews/routes/web.php
+++ mynews/routes/web.php
@@ -11,6 +11,50 @@
 |
 */
 
+/*
+PHP/Laravel10
+課題1→Routing
+課題2→Routingをグループ化するメリットは、まとめて記載できる点
+　　　コードベタ打ちでもしっかり書けば動作するだろうが、
+　　　拡張性、再利用性、独立性を担保しながら複数人での開発を想定するなら
+　　　グループ化はメリットがあると考えられる。
+*/
+
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
+
+Route::get('XXX', 'AAAController@bbb');
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+});
+
+
+
+
+/*
+メモ
+ http://XXXXXX.jp/admin/をRoutingする場合、
+ Route::group(['prefix' => 'admin'], function() {
+     Route::get('news/create', 'Admin\NewsController@add');
+ });
+●Route::group→複数のRouting設定をグループ化する役割
+●['prefix' => 'admin']→無名関数function(){}の中の設定のurlを http://XXXXXX.jp/　　　　　　　　　　　admin/にしている
+●Route::get('news/create', 'Admin\NewsController@add')について
+　・news/create→このケースではadmin以下のurl。これで、
+　　　　　　　  http://XXXXXX.jp/admin/news/create/となる
+　・NewsController→コントローラー名
+　・@add→コントローラーのアクション
+　・\Adminはディレクトリの位置か？
+
+まとめると
+Route::get('アクセス先','ディレクトリ\コントローラー名@アクション名');
+*/
