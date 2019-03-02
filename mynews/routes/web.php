0dab4a16ab16ad8e2681a72c296498e4773f11d5
”YuYuYuhei” <yuuhei.kubo7@gmail.com>
2019-03-02 15:32:49 +0900

    PHP/Laravel14-homework

--- mynews/routes/web.php
+++ mynews/routes/web.php
@@ -26,18 +26,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-});
-
-Route::get('XXX', 'AAAController@bbb');
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
 });
-
-
 
 
 /*
